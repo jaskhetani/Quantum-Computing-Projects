2145d05ec18ca7673c8be7fbafb903f55fc48627
Jas Khetani <jaskhetani1981@gmail.com>
2024-06-15 20:13:12 +0000

    modified:   Bloch Sphere Simulator/gates.py

--- Bloch Sphere Simulator/gates.py
+++ Bloch Sphere Simulator/gates.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 # X Gate: |0〉to |1〉(x-axis rotation)
-def X(intensity=100, oldx=0, oldy=0, oldz=0):
+def X(intensity=100, oldx=1, oldy=0, oldz=0):
     x, y, z, theta, phi = oldx, oldy, oldz, np.pi*(intensity/100), -(np.pi / 2)
     x += np.sin(theta) * np.cos(phi)
     y += np.sin(theta) * np.sin(phi)
@@ -12,7 +12,7 @@ def X(intensity=100, oldx=0, oldy=0, oldz=0):
     return x, y, z
 
 # Y Gate: |0〉to |1〉(y-axis rotation)
-def Y(intensity=100, oldx=0, oldy=0, oldz=0):
+def Y(intensity=100, oldx=1, oldy=0, oldz=0):
     x, y, z, theta, phi = oldx, oldy, oldz, np.pi*(intensity/100), 0
     x += np.sin(theta) * np.cos(phi)
     y += np.sin(theta) * np.sin(phi)
@@ -20,7 +20,7 @@ def Y(intensity=100, oldx=0, oldy=0, oldz=0):
     return x, y, z
 
 # Z Gate: |+〉to |-〉(z-axis rotation)
-def Z(intensity=100, oldx=0, oldy=0, oldz=0):
+def Z(intensity=100, oldx=1, oldy=0, oldz=0):
     x, y, z, theta, phi = oldx, oldy, oldz, 0, np.pi*(intensity/100)
     x += np.sin(theta) * np.cos(phi)
     y += np.sin(theta) * np.sin(phi)
@@ -28,11 +28,11 @@ def Z(intensity=100, oldx=0, oldy=0, oldz=0):
     return x, y, z
 
 # H Gate: |0〉to |+〉(xz-axis rotation)
-def H(intensity=100, oldx=0, oldy=0, oldz=0):
+def H(intensity=100, oldx=1, oldy=0, oldz=0):
     return oldz, oldy, oldx
 
 # S Gate: |+〉to |+i〉(z-axis rotation)
-def S(intensity=100, oldx=0, oldy=0, oldz=0):
+def S(intensity=100, oldx=1, oldy=0, oldz=0):
     x, y, z, theta, phi = oldx, oldy, oldz, 0, (np.pi/2)*(intensity/100)
     x += np.sin(theta) * np.cos(phi)
     y += np.sin(theta) * np.sin(phi)
@@ -40,7 +40,7 @@ def S(intensity=100, oldx=0, oldy=0, oldz=0):
     return x, y, z
 
 # antiS Gate: |+〉to |-i〉(z-axis rotation)
-def antiS(intensity=100, oldx=0, oldy=0, oldz=0):
+def antiS(intensity=100, oldx=1, oldy=0, oldz=0):
     x, y, z, theta, phi = oldx, oldy, oldz, 0, -(np.pi/2)*(intensity/100)
     x += np.sin(theta) * np.cos(phi)
     y += np.sin(theta) * np.sin(phi)
@@ -48,7 +48,7 @@ def antiS(intensity=100, oldx=0, oldy=0, oldz=0):
     return x, y, z
 
 # T Gate: Half of S Gate (z-axis rotation)
-def T(intensity=100, oldx=0, oldy=0, oldz=0):
+def T(intensity=100, oldx=1, oldy=0, oldz=0):
     x, y, z, theta, phi = oldx, oldy, oldz, 0, (np.pi/4)*(intensity/100)
     x += np.sin(theta) * np.cos(phi)
     y += np.sin(theta) * np.sin(phi)
@@ -56,7 +56,7 @@ def T(intensity=100, oldx=0, oldy=0, oldz=0):
     return x, y, z
 
 # antiT Gate: Half of antiS Gate (z-axis rotation)
-def antiT(intensity=100, oldx=0, oldy=0, oldz=0):
+def antiT(intensity=100, oldx=1, oldy=0, oldz=0):
     x, y, z, theta, phi = oldx, oldy, oldz, 0, -(np.pi/4)*(intensity/100)
     x += np.sin(theta) * np.cos(phi)
     y += np.sin(theta) * np.sin(phi)
@@ -64,7 +64,7 @@ def antiT(intensity=100, oldx=0, oldy=0, oldz=0):
     return x, y, z
 
 # A Gate: Abstractly controlled by theta and phi angles (custom rotations)
-def A(theta=0, phi=0, oldx=0, oldy=0, oldz=0):
+def A(theta=0, phi=0, oldx=1, oldy=0, oldz=0):
     x, y, z = oldx, oldy, oldz
     x += np.sin(theta) * np.cos(phi)
     y += np.sin(theta) * np.sin(phi)
